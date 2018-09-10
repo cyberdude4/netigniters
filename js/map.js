099f188f5c1cfa98da19bef8926d8c86adda0122
vishal bondre <30455089+cyberdude4@users.noreply.github.com>
2018-09-10 13:58:05 -0700

    all remove new upload

--- js/map.js
+++ js/map.js
@@ -1,23 +1,34 @@
-function initialize() {
-	var myOptions = {
-		zoom: 16,
-		center: new google.maps.LatLng(51.489500, -0.096777), //change the coordinates
-		mapTypeId: google.maps.MapTypeId.ROADMAP,
-		scrollwheel: false,
-		mapTypeControl: false,
-		zoomControl: false,
-		streetViewControl: false,
-		styles: [{"elementType":"geometry","stylers":[{"color":"#f5f5f5"}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"elementType":"labels.text.fill","stylers":[{"color":"#616161"}]},{"elementType":"labels.text.stroke","stylers":[{"color":"#f5f5f5"}]},{"featureType":"administrative.land_parcel","elementType":"labels.text.fill","stylers":[{"color":"#bdbdbd"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#eeeeee"}]},{"featureType":"poi","elementType":"labels.text.fill","stylers":[{"color":"#757575"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#e5e5e5"}]},{"featureType":"poi.park","elementType":"labels.text.fill","stylers":[{"color":"#9e9e9e"}]},{"featureType":"road","elementType":"geometry","stylers":[{"color":"#ffffff"}]},{"featureType":"road.arterial","elementType":"labels.text.fill","stylers":[{"color":"#757575"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#dadada"}]},{"featureType":"road.highway","elementType":"labels.text.fill","stylers":[{"color":"#616161"}]},{"featureType":"road.local","elementType":"labels.text.fill","stylers":[{"color":"#9e9e9e"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"color":"#e5e5e5"}]},{"featureType":"transit.station","elementType":"geometry","stylers":[{"color":"#eeeeee"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#c9c9c9"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"color":"#9e9e9e"}]}]
-		}
-	var img_icon = 'img/map-marker.png'
-	var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
-	var marker = new google.maps.Marker({
-		map: map,
-		icon: img_icon,
-		position: new google.maps.LatLng(51.488966, -0.096777) //change the coordinates
-	});
-	google.maps.event.addListener(marker, "click", function() {
-		infowindow.open(map, marker);
-	});
-}
-google.maps.event.addDomListener(window, 'load', initialize);
+    /* ==============================================
+    MAP -->
+    =============================================== */
+    
+    var locations=[ ['<div class="infobox"><h3 class="title"><a href="#">OUR USA OFFICE</a></h3><span>NEW YORK CITY 2045 / 65</span><span>+90 555 666 77 88</span></div>',
+    52.370216,
+    4.895168,
+    2]];
+    var map=new google.maps.Map(document.getElementById('map'), {
+        zoom: 14, scrollwheel: false, navigationControl: true, mapTypeControl: false, scaleControl: false, draggable: true, styles: [ {
+            "featureType": "administrative", "elementType": "labels.text.fill", "stylers": [{"featureType":"poi.business","elementType":"geometry.fill","stylers":[{"visibility":"on"}]}]
+        }
+        ], center: new google.maps.LatLng(52.370216, 4.895168), mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    
+    );
+    var infowindow=new google.maps.InfoWindow();
+    var marker,
+    i;
+    for (i=0;
+    i < locations.length;
+    i++) {
+        marker=new google.maps.Marker( {
+            position: new google.maps.LatLng(locations[i][1], locations[i][2]), map: map, icon: 'images/loaders/loader-realestate.gif'
+        }
+        );
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                infowindow.setContent(locations[i][0]);
+                infowindow.open(map, marker);
+            }
+        }
+        )(marker, i));
+    }
